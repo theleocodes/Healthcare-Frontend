23a629d5f5b7734d00b585194a8be9799d1d6d1d
Prasoon Rajpoot <betusingh890@gmail.com>
2024-02-01 22:06:14 +0530

    doing changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,46 +1,65 @@
-import React from 'react'
+import React from "react";
+import "./home.css";
+
+import AccessibilityIcon from '@mui/icons-material/Accessibility';
+import MonitorHeartIcon from '@mui/icons-material/MonitorHeart';
+import RowingIcon from '@mui/icons-material/Rowing';
 
 function Home() {
   return (
-    <div>
-
-
-        <div>
+    <div className="main-div">
+      <div className="top-section">
         {/* give this div a class , and make  its display property as  flex */}
-            <div>
-                <h2>Yoga for mind</h2>
-                <span>watch our lead yoga <u> jdklfjalkds </u></span>
-            </div>
-            <div>
-                {/* video */}
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/hJbRpHZr_d0?si=CY1nrux3uJEwB5ZT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </div>
-
+        <div className="top-left">
+          <h2>Yoga for mind</h2>
+          <span>
+            watch our lead yoga therapist,<u>Ms Uma Subramaniyam </u> expalin
+            Yoga for mind
+          </span>
         </div>
-         <span>is this for you</span>
+        <div className="top-right">
+          {/* video */}
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/hJbRpHZr_d0?si=CY1nrux3uJEwB5ZT"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
+      <span>is this for you</span>
+      <div className="second-section">
+        {/* give this div a class , and make  its display property as  flex */}
         <div>
-           {/* give this div a class , and make  its display property as  flex */}
-            <div>
-                {/* //any icon */}
-                <p>hard to feel</p>
-                {/* study diff between div, span , paragraph tag */}
-                <button>Book trial</button>
-            </div>
-            <div>
-                {/* //any icon */}
-                <p>hard to feel</p>
-                {/* study diff between div, span , paragraph tag */}
-                <button>Book trial</button>
-            </div>
-            <div>
-                {/* //any icon */}
-                <p>hard to feel</p>
-                {/* study diff between div, span , paragraph tag */}
-                <button>Book trial</button>
-            </div>
+          <div className="image-circle">
+            <AccessibilityIcon />
+          </div>
+          <p>hard to feel calm or sleep because of stress</p>
+          {/* study diff between div, span , paragraph tag */}
+          <button>Book trial</button>
         </div>
+        <div>
+          <div className="image-circle">
+            <MonitorHeartIcon />
+          </div>
+          <p>If you have palpatations and breathing troubles</p>
+          {/* study diff between div, span , paragraph tag */}
+          <button>Book trial</button>
+        </div>
+        <div>
+          <div className="image-circle">
+            <RowingIcon />
+          </div>
+          <p>You have been advised by doctors to avoid physical activity</p>
+          {/* study diff between div, span , paragraph tag */}
+          <button>Book trial</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,32 +1,76 @@
-import React from 'react'
-
+import React from "react";
+import "./product.css";
 
 function Product() {
   return (
-    <div>
-        <div>
-            <h2>Yoga for Mind</h2>
-            <div id = "imageContainer">
-                <img src = "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F615316329%2F1812914394463%2F1%2Foriginal.20231007-072214?w=940&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C92%2C800%2C400&s=eb01d3b4211cbda1cb56560c87015e58">
-                </img>
-            </div>
-            <div>
-                <h2>What is Yoga for Mind?</h2>
-                <p>The Yoga for Mind Sessions involve minimal physical movement and relies on techniques such as Pranayama and Meditation.
-                Suited for women who cannot perform more physical forms of yoga and are looking for relief from concerns such as stress, anxiety, low mood etc.
-                </p>
-            </div>
-        </div>
-        {/* div for choose package */}
-        <div>
-            <h2>Choose your package</h2>
-            <div></div>
+    <div className="main-div">
+      <div className="left-section">
+        <h2>Yoga for Mind</h2>
+        <div id="imageContainer">
+          <img
+            className="up-img"
+            src="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F615316329%2F1812914394463%2F1%2Foriginal.20231007-072214?w=940&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C92%2C800%2C400&s=eb01d3b4211cbda1cb56560c87015e58"
+          ></img>
         </div>
         <div>
+          <h2>What is Yoga for Mind?</h2>
+          <p>
+            The Yoga for Mind Sessions involve minimal physical movement and
+            relies on techniques such as Pranayama and Meditation. Suited for
+            women who cannot perform more physical forms of yoga and are looking
+            for relief from concerns such as stress, anxiety, low mood etc.
+          </p>
+        </div>
+      </div>
 
+      {/* div for choose package */}
+      <div className="right-section">
+        <h2>Choose your package</h2>
+        <div className="one-row">
+          <div className="right-left">
+            <h2> 6 Months</h2>
+            <del className="old-price">&#8377;12999</del>
+            <span className="new-price">&#8377;5499</span>
+          </div>
+          <div className="right-right">
+            <span>916/month</span>
+            <br />
+            <button>Buy Now</button>
+          </div>
         </div>
+
+        <div className="one-row">
+          <div className="right-left">
+            <h2> 3 Months</h2>
+            <del className="old-price">&#8377;12999</del>
+            <span className="new-price">&#8377;5499</span>
+          </div>
+          <div className="right-right">
+            <span>916/month</span>
+            <br />
+            <button>Buy Now</button>
+          </div>
+        </div>
+
+        <div className="one-row">
+          <div className="right-left">
+            <h2> 1 Months</h2>
+            <del className="old-price">&#8377;12999</del>
+            <span className="new-price">&#8377;5499</span>
+          </div>
+          <div className="right-right">
+            <span>916/month</span>
+            <br />
+            <button>Buy Now</button>
+          </div>
+        </div>
+
+        <h2>What you'll get</h2>
+        <p> <span>&#xf075;</span> A chat group on the elda app where you can connect with your trainer and rest of the Elda yoga community </p>
+        <p><span></span></p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Product
+export default Product;
